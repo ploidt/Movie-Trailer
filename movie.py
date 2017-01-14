9eb9485e33aac6d61ecaa9427a9e8ebe25092c97
ploidt <ploid@Ploid-Macbook.local>
2017-01-14 16:08:29 +0700

    add __doc__ to Movie class

--- movie.py
+++ movie.py
@@ -1,12 +1,14 @@
 import webbrowser
 
 class Movie():
-    #crete an init of Movie class
+    """This class provides a way to store movie related information"""
+    
+    # crete an init of Movie class
     def __init__(self,movie_title,movie_poster,movie_trailer_url):
         self.title = movie_title
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer_url
 
-    #create a method that will open the trailer in the web browser
+    # create a method that will open the trailer in the web browser
     def show_trailer(self):
         webbrowser.open(trailer_youtube_url)
